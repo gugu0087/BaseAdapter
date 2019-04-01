516de321232f79bb59068744c8e35a96100c9068
许运昌 <xuyunchang@mzjmedia.net>
2019-04-01 12:14:09 +0800

    2.3

---
+++ app/src/main/java/com/doctor/mds/myapplication/DataModel.java
@@ -0,0 +1,17 @@
+package com.doctor.mds.myapplication;
+
+/**
+ * Create by Admin on 2019/4/1
+ */
+public class DataModel {
+    private String name;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
